b7026c250ce9ca4d49ae947a328f5bbccf83eea5
vivekpansari <vivekpansari@gmail.com>
2020-05-15 11:52:46 +0530

    first commit

---
+++ PAO/PAO.aspx
@@ -0,0 +1,213 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/oceans.Master" CodeBehind="PAO.aspx.vb" Inherits="oceansV2.PAO" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <cc1:RoundedCornersExtender ID="RoundedCornersExtender1" runat="server"  TargetControlID="heading" Radius="10" />
+    <div id="heading" runat="server" align="center" style="background-color:darkorchid;">
+       <p style="color :white ">PAO Detail</p> 
+    </div>
+    <div id="entry" align="center" >
+
+        <table id="tblsanction" border="1" runat="server">
+           
+            <tr>
+
+
+                <td align="left">
+
+                    <asp:Label ID="lblreldatefrom" runat="server" Text="Release Date From"></asp:Label></td>
+                <td style="width: 100px" align="left">
+                    <asp:TextBox ID="txtfromdate" runat="server"></asp:TextBox>
+
+                    <asp:RegularExpressionValidator ID="valefromdate" runat="server" ControlToValidate="txtfromdate"
+                        Display="Dynamic" ErrorMessage="Input Proper Date(dd/mm/yyyy)" ValidationExpression="^([0-3]\d)/([01]\d)/(\d{4})$"></asp:RegularExpressionValidator>
+                    <cc1:calendarextender id="CalendarExtender1" runat="server" format="dd/MM/yyyy" targetcontrolid="txtfromdate" popupposition="TopLeft">
+                    </cc1:calendarextender>
+                    <asp:CompareValidator ID="valcfromdate" Display="Dynamic" runat="server" Type="Date" Operator="LessThanEqual"  ControlToValidate="txtfromdate" ErrorMessage="Date Should't Be Greater Than Today's Date"></asp:CompareValidator>
+
+                    <%--  <asp:TextBox ID="TxtFinYear" runat="server" ReadOnly="true" Enabled="False"></asp:TextBox>--%>
+
+                    <%-- <asp:RequiredFieldValidator ID="valryear" runat="server" ErrorMessage="*" ControlToValidate="TxtFinYear" Display="Dynamic" InitialValue="Select Year" >*</asp:RequiredFieldValidator>--%>
+
+                    
+                </td>
+            </tr>
+
+
+
+
+            <tr>
+                <td>
+                    <asp:Label ID="lblscheme" runat="server" Text="Select Scheme"></asp:Label></td>
+                <td align="left">
+                    <asp:DropDownList ID="ddlschemecode" runat="server" AutoPostBack="True">
+                       
+                    </asp:DropDownList>
+                    
+                </td>
+            </tr>
+
+            <tr>
+
+                <td colspan="2">
+
+                    <asp:Button ID="btngenerate" runat="server" Text="Proceed" CssClass="btn btn-info" />
+                </td>
+
+            </tr>
+            <tr>
+
+                <td colspan="2">
+                    <asp:Label ID="lblmsg" runat="server"></asp:Label></td>
+            </tr>
+        </table>
+        
+        <div style="margin-bottom: 10px">
+        </div>
+
+        <asp:GridView ID="GridView4" runat="server" AutoGenerateColumns="False"
+            OnSelectedIndexChanged="GridView4_SelectedIndexChanged" Visible="False" AllowPaging="true">
+            <pagersettings mode="NumericFirstLast"
+          position="Bottom"           
+          pagebuttoncount="8" />
+            
+                      
+        <pagerstyle backcolor="LightBlue"
+          height="30px"
+          verticalalign="Bottom"
+          horizontalalign="Center"/>
+            <Columns>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="btnSelect" runat="server" CausesValidation="true" CommandName="Select"
+                            Text="Select" CommandArgument='<%# Eval("RecomId") %>'></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="RecomID" HeaderText="RecomId" />
+                <asp:BoundField DataField="YearCode" HeaderText="YearCode" />
+
+
+                <asp:TemplateField HeaderText="Category" Visible="False">
+                    <ItemTemplate>
+                        <asp:Label ID="lblcategory" runat="server"></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="RecomDate" HeaderText="Release Date" DataFormatString="{0:dd/MM/yy}" />
+                <asp:BoundField DataField="RecomAmount" Visible="False" HeaderText="RecomAmount" />
+                <asp:BoundField DataField="RecomLoanAmt" Visible="False" HeaderText="RecomLoanAmt" />
+                <asp:BoundField DataField="RecomGrantAmt" Visible="False" HeaderText="RecomGrantAmt" />
+                <asp:BoundField DataField="ReleaseAmount" HeaderText="ReleaseAmount" />
+                <asp:BoundField DataField="ReleaseLoanAmount" Visible="False" HeaderText="ReleaseLoanAmount" />
+                <asp:BoundField DataField="ReleaseGrantAmount" Visible="False" HeaderText="ReleaseGrantAmount" />
+                <asp:BoundField DataField="SanctionNo" HeaderText="SanctionNo" />
+                <asp:BoundField DataField="RecomStatus" Visible="False" HeaderText="RecomStatus" />
+                <asp:BoundField DataField="RecomDocument" Visible="False" HeaderText="RecomDocument" />
+                <asp:BoundField DataField="RecomFresh" Visible="False" HeaderText="RecomFresh" />
+                <asp:BoundField DataField="RecomYesNo" Visible="False" HeaderText="RecomYesNo" />
+                <asp:BoundField DataField="RecomForwardTo" Visible="False" HeaderText="RecomForwardTo" />
+                <asp:BoundField DataField="PAOApproved" Visible="False" HeaderText="PAOApproved" />
+                <asp:BoundField DataField="SchemeName" HeaderText="SchemeName" />
+                <asp:BoundField DataField="IGANo" HeaderText="IGA No" Visible="False" />
+                <asp:BoundField DataField="PAOApprovedDate" HeaderText="PAO Approved Date" Visible="False" />
+
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="LnkbtnAccept4" runat="server"
+                            Text="Enter Data" CausesValidation="false" CommandName="Accept" CommandArgument='<%# Eval("RecomId") %>'></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="LnkbtnReject4" runat="server"
+                            Text="Reject" CausesValidation="false" CommandName="Reject" CommandArgument='<%# Eval("RecomId") %>'></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+
+        <asp:GridView ID="GrdReleaseDetail" runat="server" AutoGenerateColumns="False"
+            Visible="False" AllowPaging="true" PageSize="10">
+            <Columns>
+                <asp:BoundField DataField="StateName" HeaderText="StateName" />
+                <asp:BoundField DataField="RecomDetailLoanAmount" HeaderText="RecomDetailLoanAmount" />
+                <asp:BoundField DataField="RecomDetailGrantAmount" HeaderText="RecomDetailGrantAmount" />
+                <asp:BoundField DataField="ReleaseDetailLoanAmount" HeaderText="ReleaseDetailLoanAmount" />
+                <asp:BoundField DataField="ReleaseDetailGrantAmount" HeaderText="ReleaseDetailGrantAmount" />
+                <asp:BoundField DataField="Granthead" HeaderText="Grant head" />
+                <asp:BoundField DataField="Loanhead" HeaderText="Loan head" />
+            </Columns>
+        </asp:GridView>
+
+
+        <div>
+            <asp:Literal ID="litmsg" runat="server" EnableViewState="false" Visible="false"></asp:Literal>
+        </div>
+        <div>
+            <asp:Button runat="server" ID="btnback" Text="Back to Previous Data." Visible="false" />
+        </div>
+
+        <asp:Label runat="server" Visible="false" Text="" ID="lblaccept"></asp:Label>
+        <div runat="server" id="divAccept" align="center" width="80px" height="100px" visible="false" style="border: 1px">
+
+            <table border="2">
+                <tr>
+                    <td>IGA No  
+                    </td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtIGA" EnableViewState="false"></asp:TextBox>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align="left" style="width: 100px">
+                        <asp:Label ID="lblReleaseDate" runat="server" Text="Release Date From PAO"></asp:Label></td>
+                    <td style="width: 100px" align="left">
+                        <asp:TextBox ID="txtPAOfromdate" runat="server"></asp:TextBox>
+                        <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ControlToValidate="txtPAOfromdate"
+                            Display="Dynamic" ErrorMessage="Input Proper Date(dd/mm/yyyy)" ValidationExpression="^([0-3]\d)/([01]\d)/(\d{4})$"></asp:RegularExpressionValidator>
+                        <cc1:CalendarExtender id="CalendarExtender2" runat="server" format="dd/MM/yyyy" targetcontrolid="txtPAOfromdate" popupposition="TopLeft"></cc1:CalendarExtender>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td style="margin-left: 20px; height: 17px;">
+                        <asp:Button ID="btnSavePAO" runat="server" Text="Save" Style="height: 26px" />
+                    </td>
+                    <td>
+                        <asp:Button ID="btnCancelPAO" runat="server" Text="Cancel" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+        <div runat="server" id="divRemark" align="center" style="width: 160px;" visible="false">
+            <table border="3">
+                <tr>
+                    <td>Reason  
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtReject" runat="server" TextMode="MultiLine"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="margin-left: 20px; height: 17px;">
+                        <asp:Button ID="BtnSave" runat="server" Text="Submitt" Style="height: 26px" />
+                    </td>
+                    <td>
+                        <asp:Button ID="btnCancel" runat="server" Text="Cancel" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+
+
+
+
+        <br />
+
+
+    </div>
+</asp:Content>
---
+++ PAO/PAO.aspx.vb
@@ -0,0 +1,330 @@
+﻿Imports System.IO
+Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+Imports System.Collections
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports System.Net
+Imports System.Net.Mail
+Public Class PAO
+    Inherits BaseClass
+    Dim releaseamt As Double
+    Dim SchemeType As String
+    Dim FCDYes As Boolean
+    Dim FCDVal As String
+    Dim dt As DataTable
+    Dim releasedate As DateTime
+    Dim releasedate1 As DateTime
+    Dim diff As Integer
+    Dim todaydate As DateTime
+    Dim sanctionid As Integer
+    Dim iNumberOfDays As Integer
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        MyBase.LogActivity("oceans", True)
+        lblmsg.Text = ""
+        Try
+            valcfromdate.ValueToCompare = System.DateTime.Now.ToShortDateString
+            If Not Page.IsPostBack Then
+
+                If User.IsInRole("PAO") Then
+                    Session("Role") = "PAO"
+                    SchemeType = "SPS"
+                    FCDYes = False
+                End If
+                Session("FCDYes") = FCDYes
+                Session("YearCode") = "2018-2019"
+
+
+
+                ddlschemecode.DataSource = DAL.GetDataBySchemeTypeYearCode("SPS", "2018-2019")
+                ddlschemecode.DataTextField = "SchemeName"
+                ddlschemecode.DataValueField = "SchemeCode"
+                ddlschemecode.DataBind()
+                ddlschemecode.Items.Insert(0, "Select Scheme")
+
+                'End Code
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Protected Sub btngenerate_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btngenerate.Click
+        GridView4.Visible = True
+
+        If ddlschemecode.SelectedIndex <> 0 And txtfromdate.Text <> "" Then
+            dt = DAL.BindPAOGridDatewise(1, ddlschemecode.SelectedValue.ToString(), txtfromdate.Text)
+        ElseIf ddlschemecode.SelectedIndex <> 0 And txtfromdate.Text = "" Then
+
+            dt = DAL.BindPAOGridDatewise(2, ddlschemecode.SelectedValue.ToString(), DateTime.Now)
+        ElseIf ddlschemecode.SelectedIndex = 0 And txtfromdate.Text <> "" Then
+            dt = DAL.BindPAOGridDatewise(3, "", txtfromdate.Text)
+        End If
+
+
+        GridView4.DataSource = dt
+        GridView4.DataBind()
+        If GridView4.Rows.Count > 0 Then
+            GridView4.Visible = True
+            tblsanction.Visible = True
+
+
+        Else
+            lblmsg.Text = "No Release Available For This Scheme"
+
+        End If
+
+    End Sub
+
+    Protected Sub bindGridView4()
+        If ddlschemecode.SelectedIndex <> 0 And txtfromdate.Text <> "" Then
+            dt = DAL.BindPAOGridDatewise(1, ddlschemecode.SelectedValue.ToString(), txtfromdate.Text)
+        ElseIf ddlschemecode.SelectedIndex <> 0 And txtfromdate.Text = "" Then
+
+            dt = DAL.BindPAOGridDatewise(2, ddlschemecode.SelectedValue.ToString(), DateTime.Now)
+        ElseIf ddlschemecode.SelectedIndex = 0 And txtfromdate.Text <> "" Then
+            dt = DAL.BindPAOGridDatewise(3, "", txtfromdate.Text)
+        End If
+
+        GridView4.DataSource = dt
+        GridView4.DataBind()
+
+    End Sub
+    Protected Sub GridView4_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles GridView4.SelectedIndexChanged
+        ViewState("RecomId") = GridView4.Rows(GridView4.SelectedIndex).Cells(1).Text
+        dt = DAL.BindPAOReleaseDetail(ViewState("RecomId"))
+        GrdReleaseDetail.DataSource = dt
+        GrdReleaseDetail.DataBind()
+        GridView4.Visible = False
+        GrdReleaseDetail.Visible = True
+
+        btnback.Visible = True
+    End Sub
+    Protected Sub btnback_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnback.Click
+        If ddlschemecode.SelectedValue.ToString() = "Select Scheme" Then
+            dt = DAL.BindPAOGridwithdate(Session("YearCode"), txtfromdate.Text)
+        Else
+            dt = DAL.BindPAOGrid(Session("YearCode"), ddlschemecode.SelectedValue.ToString())
+        End If
+        lblaccept.Visible = False
+        GridView4.DataSource = dt
+        GridView4.DataBind()
+        GridView4.Visible = True
+        GrdReleaseDetail.Visible = False
+        btnback.Visible = False
+
+    End Sub
+
+    Protected Sub GridView4_RowCommand(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewCommandEventArgs) Handles GridView4.RowCommand
+        Try
+
+            Dim LnkbtnAccept As LinkButton = GridView4.FindControl("LnkbtnAccept4")
+            Dim LnkbtnReject As LinkButton = GridView4.FindControl("LnkbtnReject4")
+            Dim PAOStatus As DataTable
+            Dim IGANo As String
+            Dim PAOApprovedDate As String
+            Dim IGAdt As DataTable
+
+            ViewState("RecomId") = Convert.ToInt32(e.CommandArgument)
+            If ddlschemecode.SelectedValue.ToString() = "" Then
+                dt = DAL.BindPAOGridbyrecomid(Session("YearCode"), ViewState("RecomId"))
+            Else
+
+                dt = DAL.BindPAOGrid(Session("YearCode"), ddlschemecode.SelectedValue.ToString())
+            End If
+
+            releasedate = dt.Rows(0).Item("RecomDate")
+
+            If dt.Rows.Count > 0 Then
+                If e.CommandName = "Accept" Then
+                    IGAdt = DAL.FetchIGAData(ViewState("RecomId"))
+                    IGANo = IGAdt.Rows(0).Item("IGANo").ToString()
+                    PAOApprovedDate = IGAdt.Rows(0).Item("PAOApprovedDate").ToString()
+
+                    If IGANo <> "" And PAOApprovedDate <> "" Then
+                        lblaccept.Text = "Record Already Entered."
+                        lblaccept.ForeColor = System.Drawing.Color.Green
+                        lblaccept.Font.Bold = True
+                        lblaccept.Font.Size = 18
+                        lblaccept.Visible = True
+                        GridView4.Visible = False
+                        btnback.Visible = True
+                    Else
+                        divAccept.Visible = True
+                        GridView4.Visible = False
+                    End If
+
+                ElseIf e.CommandName = "Reject" Then
+                    PAOStatus = DAL.InsertPAOStatus(ViewState("RecomId").ToString(), "R", System.DateTime.Now(), "N")
+                    dt = DAL.BindPAOGrid(Session("YearCode"), ddlschemecode.SelectedValue.ToString())
+                    GridView4.DataSource = dt
+                    GridView4.DataBind()
+                    txtReject.Text = ""
+                    divRemark.Visible = True
+                    GridView4.Visible = False
+                    divAccept.Visible = False
+
+                End If
+            End If
+
+
+            'iNumberOfDays = DetermineNumberofDays(releasedate)
+            'DAL.InsertDateDifference(ViewState("RecomId"), iNumberOfDays)
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Function DetermineNumberofDays(ByVal releasedate As DateTime) As Integer
+        Dim tsTimeSpan As TimeSpan
+        tsTimeSpan = Now.Subtract(releasedate)
+        iNumberOfDays = tsTimeSpan.Days
+        Return iNumberOfDays
+    End Function
+
+    Protected Sub GridView4_RowDataBound1(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewRowEventArgs) Handles GridView4.RowDataBound
+        If e.Row.RowType = DataControlRowType.DataRow Then
+
+            ViewState("SchemeName") = DataBinder.Eval(e.Row.DataItem, "SchemeName")
+
+            'lbSchemeName.Text = ViewState("SchemeName").ToString()
+            If (e.Row.DataItem("PAOApproved").ToString = "R") Then
+                e.Row.BackColor = Drawing.Color.DeepSkyBlue
+            End If
+
+            If (e.Row.DataItem("PAOApproved").ToString = "O") Then
+                e.Row.BackColor = Drawing.Color.LightSkyBlue
+            End If
+
+        End If
+
+        txtIGA.Text = ""
+        txtPAOfromdate.Text = ""
+
+    End Sub
+
+
+
+    Protected Sub GrdReleaseDetail_PageIndexChanging(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewPageEventArgs) Handles GrdReleaseDetail.PageIndexChanging
+        GrdReleaseDetail.PageIndex = e.NewPageIndex
+        ViewState("RecomId") = GridView4.Rows(GridView4.SelectedIndex).Cells(1).Text
+        dt = DAL.BindPAOReleaseDetail(ViewState("RecomId"))
+        GrdReleaseDetail.DataSource = dt
+        GrdReleaseDetail.DataBind()
+        GridView4.Visible = False
+        GrdReleaseDetail.Visible = True
+        btnback.Visible = True
+    End Sub
+
+
+    Protected Sub BtnSave_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles BtnSave.Click
+        'Insert the reason of cancellation
+        Try
+            DAL.InsertRemark(ViewState("RecomId"), txtReject.Text)
+            divRemark.Visible = False
+
+            dt = DAL.BindPAOGrid(Session("YearCode"), ddlschemecode.SelectedValue.ToString())
+            If dt.Rows.Count > 0 Then
+                GridView4.DataSource = dt
+                GridView4.DataBind()
+                GridView4.Visible = True
+            Else
+                lblaccept.Text = "No Sanction available."
+                lblaccept.Visible = True
+
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+    Protected Sub btnCancel_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnCancel.Click
+        'Redirect  to grid
+        Try
+            DAL.InsertPAOStatus(ViewState("RecomId").ToString(), "F", System.DateTime.Now(), "Y")
+
+            'dt = DAL.BindPAOGrid(Session("YearCode"), ddlschemecode.SelectedValue.ToString())
+            If ddlschemecode.SelectedValue.ToString() = "Select Scheme" Then
+                dt = DAL.BindPAOGridwithdate(Session("YearCode"), txtfromdate.Text)
+            Else
+                dt = DAL.BindPAOGrid(Session("YearCode"), ddlschemecode.SelectedValue.ToString())
+            End If
+            If dt.Rows.Count > 0 Then
+                GridView4.DataSource = dt
+                GridView4.DataBind()
+                GridView4.Visible = True
+                divRemark.Visible = False
+
+            Else
+                lblaccept.Text = "No Sanction available."
+                lblaccept.Visible = True
+                divRemark.Visible = False
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+    Protected Sub btnSavePAO_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSavePAO.Click
+        'Insert 
+        Try
+
+            DAL.InsertPAOAcceptedStatus(ViewState("RecomId"), txtIGA.Text, Convert.ToDateTime(txtPAOfromdate.Text))
+            lblaccept.Text = "Data Saved Successfully."
+
+            lblaccept.Visible = True
+            divRemark.Visible = False
+
+
+            divAccept.Visible = False
+
+        Catch ex As Exception
+            lblaccept.Text = "ERROR: Data could not be Saved."
+        End Try
+    End Sub
+
+    Protected Sub btnCancelPAO_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnCancelPAO.Click
+        'Redirect  to grid
+        Try
+            DAL.InsertPAOStatus(ViewState("RecomId").ToString(), "F", System.DateTime.Now(), "Y")
+
+            If ddlschemecode.SelectedValue.ToString() = "Select Scheme" Then
+                dt = DAL.BindPAOGridwithdate(Session("YearCode"), txtfromdate.Text)
+            Else
+                dt = DAL.BindPAOGrid(Session("YearCode"), ddlschemecode.SelectedValue.ToString())
+            End If
+
+            If dt.Rows.Count > 0 Then
+                GridView4.DataSource = dt
+                GridView4.DataBind()
+                GridView4.Visible = True
+                divAccept.Visible = False
+
+            Else
+                lblmsg.Text = "No Sanction available."
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Protected Sub GridView4_PageIndexChanging(sender As Object, e As GridViewPageEventArgs) Handles GridView4.PageIndexChanging
+        GridView4.PageIndex = e.NewPageIndex
+        bindGridView4()
+    End Sub
+End Class
